8f635a04c3b1111b47d7e8130e4a1596432c0940
shirayanagihiroshi <shirayanagi.hiroshi@gmail.com>
2024-03-27 14:17:33 +0900

    アプリバージョンアップ

--- public/js/skt.appVersion.js
+++ public/js/skt.appVersion.js
@@ -7,7 +7,7 @@ skt.appVersion = (function () {
   'use strict';
 
   var initModule, show, zenkiP, koukiP,
-      ver = '1.8';
+      ver = '1.9';
 
   initModule      = function () {};
 
@@ -16,19 +16,19 @@ skt.appVersion = (function () {
   };
 
   zenkiP = function () {
-    return { startY : 2023, // 欠課時数の集計の期間を指定
+    return { startY : 2024, // 欠課時数の集計の期間を指定
              startM : 4,
              startD : 1,
-             endY   : 2023,
+             endY   : 2024,
              endM   : 9,
              endD   : 30 }
   }
 
   koukiP = function () {
-    return { startY : 2023,
+    return { startY : 2024,
              startM : 10,
              startD : 1,
-             endY   : 2024,
+             endY   : 2025,
              endM   : 3,
              endD   : 31 }
   }
